3980cae51168baf527420859e2329b0bb8f6fc21
Runner-Dad84 <vanhoogie@gmail.com>
2024-12-08 22:03:00 -0500

    install prop-types

--- src/inputfield.jsx
+++ src/inputfield.jsx
@@ -1,3 +1,9 @@
-export default function Component () {
+import { useState } from "react";
 
-};
+export default function InputComponent () {
+    const [text, setText ] = useState('');
+
+    const handleInput = (event) => {
+        setText(event.target.value)
+    }
+}
