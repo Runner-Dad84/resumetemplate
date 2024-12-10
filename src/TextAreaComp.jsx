916c442190ff428f0d552df4bd9264be87c2e178
Runner-Dad84 <vanhoogie@gmail.com>
2024-12-09 22:13:02 -0500

    Add textarea props

--- src/TextAreaComp.jsx
+++ src/TextAreaComp.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+const TextAreaComp = ( { name, type, value, onChange, className }) => {
+    return (
+         <textarea
+            id={name}
+            name={name} 
+            type={type}
+            value={value}
+            onChange={onChange}
+            rows='4'
+            column='45'
+            className={className}
+        />
+    )
+}
+
+export default TextAreaComp;
