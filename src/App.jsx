7dee4f934292bc103a1f98f55b7d3466345d0826
Runner-Dad84 <vanhoogie@gmail.com>
2024-12-15 22:09:37 -0500

    separate dv fr edu and pro fields

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     { label: 'City, State', name:'city-state', type:'text', class:'personal', },
     { label: 'Zip', name:'zip', type:'text', class:'personal', },
   ]
-  const fields = [
+  const educationFields = [
     { label: 'School', name:'school', type:'text', class: 'edu', },
     { label: 'Discipline', name:'discipline', type:'text', class: 'edu', },
     { label: 'Degree', name:'degree', type:'select', class: 'edu', 
@@ -39,14 +39,15 @@ function App() {
         {label: 'M.S.', value: 'M.S.'},
       ] },
     { label: 'Degree Desciption', name:'SchDesc', type:'message', class: '', },
+  ]
+  const professionalFields = [
     { label: 'Company', name:'company', type:'text', class:'', },
     { label: 'Title', name:'title', type:'text', class:'professional', },
     { label: 'Employment Start', name:'startDate', type:'date', class:'', },
     { label: 'Employment End', name:'endDate', type:'date', class:'', },
     { label: 'Job Description', name:'JobDesc', type:'message', class:'', },
-    
-    
   ];
+
   //Update h2 resume fields from value of input field
   const handleField = (event) => {
     const { name, value } = event.target;
@@ -124,6 +125,86 @@ return (
       </div>
     ))}
     </div>
+
+    <div className="educationl">
+    { educationFields.map((field) => (
+      <div key = {field.name}>
+        { (field.type === 'text' || field.type === 'date' ) ? (
+        <InputComponent
+          label = {field.label}
+          name = {field.name}
+          type = {field.type}
+          value = {fieldData[field.name]}
+          onChange = {handleField}
+          className = {classType}
+        /> ) : field.type === 'select' ? (
+          <SelectComponent
+          label = {field.label}
+          name = {field.name}
+          type = {field.type}
+          value = {fieldData[activeClass.name]}
+          onChange = {handleField}
+          className = {classType}
+          options = {field.options}
+        /> ) : (
+          <TextAreaComp
+          label = {field.label}
+          id = {field.name}
+          name = {field.name}
+          type = {field.type}
+          value = {fieldData[activeClass.name]}
+          onChange = {handleField}
+          className = {classType}
+        /> 
+        ) }
+        <h2
+          className={`resume-${field.class}`}
+        >{fieldData[field.name]}</h2>
+      </div>
+    ))}
+    </div>
+
+
+    <div className="professional">
+    { professionalFields.map((field) => (
+      <div key = {field.name}>
+        { (field.type === 'text' || field.type === 'date' ) ? (
+        <InputComponent
+          label = {field.label}
+          name = {field.name}
+          type = {field.type}
+          value = {fieldData[field.name]}
+          onChange = {handleField}
+          className = {classType}
+        /> ) : field.type === 'select' ? (
+          <SelectComponent
+          label = {field.label}
+          name = {field.name}
+          type = {field.type}
+          value = {fieldData[activeClass.name]}
+          onChange = {handleField}
+          className = {classType}
+          options = {field.options}
+        /> ) : (
+          <TextAreaComp
+          label = {field.label}
+          id = {field.name}
+          name = {field.name}
+          type = {field.type}
+          value = {fieldData[activeClass.name]}
+          onChange = {handleField}
+          className = {classType}
+        /> 
+        ) }
+        <h2
+          className={`resume-${field.class}`}
+        >{fieldData[field.name]}</h2>
+      </div>
+    ))}
+    </div>
+
+
+
     <button type='button' onClick={isActive}>{buttonText.value}</button>
   </form>
 )
