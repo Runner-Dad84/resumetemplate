b735a59b076d1dbba16b7317d93d4043b2cd4ce9
Runner-Dad84 <vanhoogie@gmail.com>
2024-12-16 21:25:13 -0500

    delete unused component

--- src/inputfield.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function InputComponent () {
-    const [text, setText ] = useState('');
-
-    const handleInput = (event) => {
-        setText(event.target.value)
-    }
-}
